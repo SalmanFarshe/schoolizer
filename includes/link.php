04e1b66b40ee6477065c309277a81b96e930250c
Salman Farshe <salman.zihad@involve.ai>
2025-08-31 02:52:24 +0600

    jump into database

--- includes/link.php
+++ includes/link.php
@@ -1,8 +1,5 @@
 <?php
-    $base_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://";
-    $base_url .= $_SERVER['HTTP_HOST'];
-    $base_url .= str_replace(basename($_SERVER['DOCUMENT_ROOT']), "", dirname($_SERVER['SCRIPT_NAME']));
-    define('BASE_URLS', $base_url);
+    define('BASE_URLS', 'https://salmanfarshe.xyz/schoolizer');
 ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 <link href="<?php echo BASE_URLS; ?>/assets/css/bootstrap.min.css" rel="stylesheet">
