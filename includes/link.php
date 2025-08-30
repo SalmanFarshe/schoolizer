4d57ee56c79547e19c2d5be8f4ccbeaa5278673a
Salman Farshe <salman.zihad@involve.ai>
2025-08-31 00:56:44 +0600

    edits ok

--- includes/link.php
+++ includes/link.php
@@ -1,5 +1,8 @@
 <?php
-    define('BASE_URLS', 'http://localhost/' . basename(dirname(__DIR__)) . '/');
+    $base_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://";
+    $base_url .= $_SERVER['HTTP_HOST'];
+    $base_url .= str_replace(basename($_SERVER['DOCUMENT_ROOT']), "", dirname($_SERVER['SCRIPT_NAME']));
+    define('BASE_URLS', $base_url);
 ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 <link href="<?php echo BASE_URLS; ?>assets/css/bootstrap.min.css" rel="stylesheet">
