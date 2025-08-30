8ece4383c4e550fcc00e9349f27ef212fd53d17b
Salman Farshe <salman.zihad@involve.ai>
2025-08-31 00:58:34 +0600

    edits ok

--- includes/link.php
+++ includes/link.php
@@ -5,6 +5,7 @@
     define('BASE_URLS', $base_url);
 ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-<link href="<?php echo BASE_URLS; ?>assets/css/bootstrap.min.css" rel="stylesheet">
-<link href="<?php echo BASE_URLS; ?>assets/css/bootstrap-icons.css" rel="stylesheet">
-<link href="<?php echo BASE_URLS; ?>assets/css/style.css" rel="stylesheet">
+<link href="<?php echo BASE_URLS; ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo BASE_URLS; ?>/assets/css/bootstrap-icons.css" rel="stylesheet">
+<link href="<?php echo BASE_URLS; ?>/assets/css/style.css" rel="stylesheet">
+<?php print_r($base_url); ?>
